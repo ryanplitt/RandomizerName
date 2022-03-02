e7bf5065dd9644fcccb5948a4c69f99d2ee18534
Ryan Plitt <ryan.plitt@icloud.com>
2022-03-02 07:39:31 -0700

    Initial Commit

---
+++ RandomizerName/RandomizerNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomizerNameApp.swift
+//  RandomizerName
+//
+//  Created by Ryan Plitt on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomizerNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
